3fae1c90ea00279704d5d256a38822fd0e2d3b40
MatuAcosta <acosta_matias8@hotmail.com>
2023-03-16 02:07:26 -0300

    probando

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const bot = async () => {
     const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
     await page.goto('https://www.deportick.com/');
-    const element = await page.waitForSelector('.home-events > .row > .col-md-12:nth-child(1)' );
+    const element = await page.waitForSelector('.home-events > .row > .col-md-12:nth-child(2)' );
+    const href = element.evaluate(element => element.href);
+    if(!href) console.log('NO HUBO CAMBIOS');
+    if(href) console.log('ENLACE PUESTO');
     await element.click();
-    await element.dispose();
-    //console.log('estoy aca');
-    //console.log(element); 
 
 }
 
-cron.schedule('*/10 * * * * *' , () => {
+cron.schedule('*/1  * * * *' , () => {
     console.log('running bot');
     bot();
 })
